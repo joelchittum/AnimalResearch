f75ad037dee967ce5003e473733c152eb3cbeeab
Joel <joel.chittum@gmail.com>
2019-01-12 13:55:16 -0500

    Created Base GUI
    This is the first commit with a base of application window.

---
+++ src/animalresearch/AnimalResearchMain.java
@@ -0,0 +1,43 @@
+package animalresearch;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class AnimalResearchMain {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AnimalResearchMain window = new AnimalResearchMain();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AnimalResearchMain() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 835, 502);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
